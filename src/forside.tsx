86c7538b5f6671d92737baf1f0426f5674e57763
PH4NTOM <Simonmadsen99@gmail.com>
2024-02-05 02:02:29 +0100

    Adding something new

---
+++ src/forside.tsx
@@ -0,0 +1,13 @@
+function Start() {
+    return (
+        <>
+            <div id="backgroundImageFrontpage">
+                <h1 className={"textCenter removeMarginTop"}>Hello World!</h1>
+                <p className={"textCenter"}>Dette er noget random tekst. You gotta love it baby!</p>
+            </div>
+
+        </>
+    )
+}
+
+export default Start
