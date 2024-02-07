fd23742b534c8b420197ec969e92c299c5fae3ab
PH4NTOM <Simonmadsen99@gmail.com>
2024-02-07 19:54:23 +0100

    Adding something new

--- src/forside.tsx
+++ src/forside.tsx
@@ -2,8 +2,17 @@ function Start() {
     return (
         <>
             <div className={"backgroundImageFrontpage"}>
-                <h1 className={"textCenter removeMarginTop"}>Hello World!</h1>
-                <p className={"textCenter"}>Dette er noget random tekst. You gotta love it baby!</p>
+                <ul className={"navbarHomepage"}>
+                    <li className={"navbarItemLeft"}><a href="#" className={"navbarItemA"}>Forside</a></li>
+                    <li className={"navbarItemRight marginRight80px"}><a href="#" className={"navbarItemA"}>Kontakt</a></li>
+                    <li className={"navbarItemRight"}><a href="#" className={"navbarItemA"}>Sponsorer</a></li>
+                    <li className={"navbarItemRight"}><a href="#" className={"navbarItemA"}>Galleri</a></li>
+                    <li className={"navbarItemRight"}><a href="#" className={"navbarItemA"}>Begivenheder</a></li>
+                </ul>
+                <br/>
+                <hr className={"marginTop45px"}/>
+                <p className={"textHomepageCenter"}>Kignæs Radio Sport</p>
+                <p className={"textCenterHomepageSmall"}>Radio Sport er ikke kun en hobby. Det er en måde at knytte bånd med andre. <br/> Kom og vær med i vores klub og oplev det selv!</p>
             </div>
         </>
     )
